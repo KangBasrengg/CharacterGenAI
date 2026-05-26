bebfbc988370486fa6f17dd5922a203c5dfbdb5d
MNuril303 <nurilhuda155@gmail.com>
2026-05-26 18:09:42 +0700

    fix: change oauth redirect to root to fix PKCE error

--- src/components/AuthModal.tsx
+++ src/components/AuthModal.tsx
@@ -74,7 +74,7 @@ export function AuthModal() {
       const { error } = await supabase.auth.signInWithOAuth({
         provider,
         options: {
-          redirectTo: `${window.location.origin}/auth/callback`,
+          redirectTo: `${window.location.origin}/`,
         },
       });
       if (error) throw error;
